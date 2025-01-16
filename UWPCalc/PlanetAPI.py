06775e5113f7a7a99a98de829d370f5b4d54f562
Avazar <JadinSadler+Git@Protonmail.com>
2025-01-15 17:40:47 -0800

    Renamed a few things, updated code

--- UWPCalc/PlanetAPI.py
+++ UWPCalc/PlanetAPI.py
@@ -1,14 +1,8 @@
-from enum import Enum
 import xml.etree.ElementTree as ET
-
-
-import PlanetClass
-
+import PlanetClass as pc
 
 
 class PlanetAPI:
-    Planets = {}
-
 
     def __init__(self):
         self.Planets = self.PopulatePlanetDictionaryFromXML()
@@ -18,55 +12,16 @@ class PlanetAPI:
         tree = ET.parse("item.xml")
         root = tree.getroot()
         output = {}
-        pc = PlanetClass
 
         for item in root.findall('Planet'):
-            new_planet = (pc.make_planet(item.find('Name').text,
-                                         item.find("UWP").text,
-                                         item.find("Bases").text,
-                                         item.find("TradeCodes").text.split(',').copy(),
-                                         item.find("TravelZone").text))
+
+            new_planet = pc.Planet(item.find('Name').text,
+                                item.find("UWP").text,
+                                item.find("Bases").text,
+                                item.find("TradeCodes").text.split(',').copy(),
+                                item.find("TravelZone").text)
             output.update({new_planet.Name: new_planet})
+
         return output
 
 
-    def uwpTranslator(self, uwpcode):
-        print(uwpcode)
-
-
-class TranslationDictionaries:
-    TradeCodes = {"Ag":"Agricultural",
-                "As":"Astroid",
-                "Ba":"Barren",
-                "De":"Desert",
-                "Fl":"Fluid Oceans",
-                "Ga":"Garden",
-                "Hi":"High Population",
-                "Ht":"High Tech",
-                "Ic":"Ice-Capped",
-                "In":"Industrial",
-                "Lo":"Low Population",
-                "Lt":"Low Tech",
-                "Na":"Non-Agricultural",
-                "Ni":"Non-Industrial",
-                "Po":"Poor",
-                "Ri":"Rich",
-                "Va":"Vacuum",
-                "Wa":"Water World"
-                }
-    TravelZones = {"A": "Amber Zone",
-                   "R": "Red Zone"
-                   }
-
-    def UWPLookupTable(self, uwpcode):
-        print("UWPLookupTable")
-
-
-class LetterToNumber(Enum):
-    A = 10
-    B = 11
-    C = 12
-    D = 13
-    E = 14
-    F = 15
-    X = 0
