a81eeeff6d08e4d8b58be690e76c204388821497
Aidan Nunn <aidannunn@yahoo.com>
2025-01-18 21:50:27 -0800

    Updating xml filepath for when ran as exe

--- UWPCalc/PlanetAPI.py
+++ UWPCalc/PlanetAPI.py
@@ -1,12 +1,16 @@
 import xml.etree.ElementTree
 from UWPCalc.PlanetClass import Planet
 from pathlib import Path
+import sys
 
 
 class PlanetAPI:
 
     def __init__(self):
-        self.database_path = Path(__file__).parent
+        if getattr(sys, "frozen", False):
+            self.database_path = Path(sys._MEIPASS)
+        else:
+            self.database_path = Path(__file__).parent
         self.planets_dict = self.populate_planet_dictionary_from_xml()
 
     def populate_planet_dictionary_from_xml(self):
